24d105273387415008c89d0a25cde2f38b4d271a
libingchen2000 <libingchen2000@hotmail.com>
2025-07-25 20:18:21 +0800

    07 Authentication 001 User Login, Logout and Flash Messages.

--- users/views.py
+++ users/views.py
@@ -1,7 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
 from .models import Profile
 
 # Create your views here.
+
+def loginUser(request):
+
+    if request.user.is_authenticated:
+        return redirect('profiles')
+
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            messages.error(request, 'User does not exist.')
+            # return render(request, 'users/login_register.html', {'error': 'User does not exist'})
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.error(request, 'Username or password is incorrect.')
+            # return render(request, 'users/login_register.html', {'error': 'Username or password is incorrect'})
+        
+        
+    return render(request, 'users/login_register.html')
+
+
+def logoutUser(request):
+    logout(request)
+    messages.error(request, 'User was logged out.')
+    return redirect('login')
+
 
 def profiles(request):
     profiles = Profile.objects.all()
