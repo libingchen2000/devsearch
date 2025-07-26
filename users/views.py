6bc726d47099375fe82ef7f745d8a2d570464c5d
libingchen2000 <libingchen2000@hotmail.com>
2025-07-26 11:49:34 +0800

    07 Authentication 002 User Registration

--- users/views.py
+++ users/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect 
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import CustomUserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
 
 # Create your views here.
 
 def loginUser(request):
+    page = 'login'
 
     if request.user.is_authenticated:
         return redirect('profiles')
@@ -30,14 +32,35 @@ def loginUser(request):
             messages.error(request, 'Username or password is incorrect.')
             # return render(request, 'users/login_register.html', {'error': 'Username or password is incorrect'})
         
-        
-    return render(request, 'users/login_register.html')
+    context = {'page': page}
+    return render(request, 'users/login_register.html', context)
 
 
 def logoutUser(request):
     logout(request)
     messages.error(request, 'User was logged out.')
     return redirect('login')
+
+
+def registerUser(request):
+
+    page = 'register'
+    form = CustomUserCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, 'User account was created!')
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.error(request, 'An error occurred during registration.')
+
+    context = {'page': page, 'form': form}
+    return render(request, 'users/login_register.html', context)
 
 
 def profiles(request):
